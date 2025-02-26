81b5dc452b24735980a398611a8258df318b5e02
jonathan-zhao-ics4u1-03-25 <jonathan.zhao@student.tdsb.on.ca>
2025-02-26 20:44:42 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class PracticeProblem {
 
@@ -6,4 +7,30 @@ public class PracticeProblem {
 
 	}
 
+	public static void swap(ArrayList<String> ArrL, int num, int num1){
+		String word = ArrL.get(num);
+		String word1 = ArrL.get(num1);
+			ArrL.set(num1, word);
+			ArrL.set(num, word1);
+		}
+
+	public static ArrayList<Double> createArrayList(double[] Arr) {
+		ArrayList<Double> ArrLdouble = new ArrayList<Double>();
+		for (int i = 0; i < Arr.length; i++) {
+			ArrLdouble.add(Arr[i]);
+		}
+		return ArrLdouble;
+	}
+
+	public static HashMap<String, Integer> combineParallelArrays(String[] names, int[] ages) {
+		HashMap <String, Integer> info = new HashMap <String, Integer>();
+		for (int i = 0; i < ages.length; i++) {
+			info.put(names[i], ages[i]);
+		}
+		return info;
+	} 
+
+	public static void increaseAge(HashMap<String, Integer> map, String name) {
+		map.put(name, map.get(name) + 1);
+	}
 }
